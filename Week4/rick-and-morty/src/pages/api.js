9ba5ab5588bd01f9937235ee5fbb7a51f4daade9
Yusfdemir <69212816+Yusfdemir@users.noreply.github.com>
2023-11-09 14:44:43 +0300

    Detay sayfası ve favoriler sayfası eklendi

--- Week4/rick-and-morty/src/pages/api.js
+++ Week4/rick-and-morty/src/pages/api.js
@@ -14,3 +14,17 @@ export const fetchCharacters = async () => {
   const response = await axiosInstance.get('/character');
   return response.data.results;
 };
+
+export const fetchCharacter = async (id) => {
+  const response = await axiosInstance.get(`/character/${id}`);
+  return response.data;
+};
+
+export const fetchCharacterWithSearch = async (searchText, searchBy) => {
+  const response = await axiosInstance.get(
+    `/character/?${searchBy}=${searchText}`
+  );
+  if (response.data && response.data.results) {
+    return response.data.results;
+  }
+};
--- Week4/rick-and-morty/src/pages/characters/index.js
+++ Week4/rick-and-morty/src/pages/characters/index.js
@@ -1,73 +1,121 @@
-import { useEffect, useState } from "react";
-import { fetchCharacters } from "../api";
+import { useEffect, useState } from 'react';
+import { fetchCharacterWithSearch, fetchCharacters } from '../api';
 import {
-  Autocomplete,
-  Box,
   Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  IconButton,
+  FormControl,
+  InputLabel,
   MenuItem,
+  Select,
   Stack,
   TextField,
   Typography,
-} from "@mui/material";
-import CardItem from "@/components/CardItem";
+} from '@mui/material';
+import { useRouter } from 'next/router';
+import InfoCard from '@/components/InfoCard';
+
 
 const Characters = () => {
+  const router = useRouter();
   const [characters, setCharacters] = useState([]);
-  const [selectedCharacter, setSelectedCharacter] = useState(null);
-  const getCharacters = async () => {
-    const characters = await fetchCharacters();
-    setCharacters(characters);
-  };
+  const [favorites, setFavorites] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchBy, setSearchBy] = useState('name');
+  const [errorFetch, setErrorFetch] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const fetchCharacterData = () => {
+    setLoading(true);
+    const fetchFunction = searchText
+      ? fetchCharacterWithSearch
+      : fetchCharacters;
+
+    fetchFunction(searchText, searchBy)
+      .then((charactersList) => {
+        setCharacters(charactersList);
+        setErrorFetch(null);
+      })
+      .catch((error) => {
+        setErrorFetch(error.message || 'Bir hata oluştu');
+      })
+      .finally(() => setLoading(false));
+  };
   useEffect(() => {
-    getCharacters();
+    fetchCharacterData();
+
+    const storedFavorites = localStorage.getItem('favorites');
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites));
+    }
   }, []);
 
-  const handleCharacterSelect = (e) => {
-    setSelectedCharacter(e.target.value);
-    console.log(selectedCharacter)
+  const handleClick = (id) => {
+    router.push(`/characters/${id}`);
   };
 
-//   const filteredCharacters = selectedCharacter
-//     ? characters.filter((character) => character.id === selectedCharacter.id)
-//     : characters;
+  const handleFavoriteToggle = (id) => {
+    const newFavorites = favorites.includes(id)
+      ? favorites.filter((favoriteId) => favoriteId !== id)
+      : [...favorites, id];
 
-    const filteredCharacters = selectedCharacter
-    ? characters.filter((character) => character.name.toString().toLowerCase().includes(selectedCharacter.toLowerCase().trim()))
-    : characters;
+    setFavorites(newFavorites);
+    localStorage.setItem('favorites', JSON.stringify(newFavorites));
+  };
 
+  const isFavorite = (id) => favorites.includes(id);
   return (
     <Stack spacing={2}>
-        {/* <Autocomplete
-        id="combo-box"
-        options={characters}
-        getOptionLabel={(character) => character.name}
-        onChange={handleCharacterSelect}
-        sx={{ width: 300 }}
-        size="small"
-        renderInput={(params) => <TextField {...params} label="Character" />}
-      /> */}
-       <TextField
-          id="outlined-select-currency"
-          label="Enter character name"
-          value={selectedCharacter}
-          onChange={handleCharacterSelect}
+      <Stack direction="row" spacing={2}>
+        <FormControl size="small" sx={{ width: '150px' }}>
+          <InputLabel id="demo-simple-select-label">Search By</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={searchBy}
+            label="Age"
+            onChange={(e) => setSearchBy(e.target.value)}
+          >
+            <MenuItem value="name">Name</MenuItem>
+            <MenuItem value="status">Status</MenuItem>
+            <MenuItem value="species">Species</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          label="Search"
+          variant="outlined"
+          size="small"
+          fullWidth
+          placeholder="Enter search text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={fetchCharacterData}
         >
-        </TextField>
+          Search
+        </Button>
+      </Stack>
       <Stack
         spacing={{ xs: 1, sm: 2 }}
         direction="row"
         useFlexGap
         flexWrap="wrap"
       >
-        {filteredCharacters.map((character) => (
-          <CardItem key={character.id}  character={character}/>
-        ))}
+        {errorFetch ? (
+          <Typography color="error">{errorFetch}</Typography>
+        ) : (
+          characters?.map((character) => (
+            <InfoCard
+              key={character.id}
+              character={character}
+              handleFavoriteToggle={handleFavoriteToggle}
+              isFavorite={isFavorite}
+              handleClick={handleClick}
+              loading={loading}
+            />
+          ))
+        )}
       </Stack>
     </Stack>
   );
